2887b128dcb83aa2aa00e55aa3546934f3fbd499
Aleksandr Shulman <aleks@cloudera.com>
2015-11-21 14:06:42 -0800

    Script cleanup

--- cloudera-on-centos/scripts/bootstrap-cloudera.sh
+++ cloudera-on-centos/scripts/bootstrap-cloudera.sh
@@ -32,19 +32,32 @@ JOBFUNCTION=${16}
 COMPANY=${17}
 INSTALLCDH=${18}
 
-CLUSTERNAME=$NAMEPREFIX
+CLUSTERNAME=${NAMEPREFIX}
+NAMESUFFIX=`echo $NAMESUFFIX | sed 's/^[^.]*\.//'`
+
+# Necessary for adding these hosts to Cloudera's internal DNS
+CLOUDERA_DNS_IP="10.17.181.104"
+CLOUDERA_DOMAIN="azure.cloudera.com"
 
 execname=$0
 log() {
   echo "$(date): [${execname}] $@" 
 }
 
-NAMESUFFIX=`echo $NAMESUFFIX | sed 's/^[^.]*\.//'`
-echo "NAMESUFFIX is: ${NAMESUFFIX}" >> /tmp/bc_initlog.out
-
-# Add this host's private ip and private hostname to the dns
-CLOUDERA_DNS_IP="10.17.181.104"
-CLOUDERA_DOMAIN="azure.cloudera.com"
+addPrivateIpToNodes() {
+  
+  ${publicHostname}=${1}
+  privateIp=$(ssh -o "StrictHostKeyChecking=false" systest@${publicHostname} -x 'sudo ifconfig | grep inet | cut -d" " -f 12 | grep "addr:1" | grep -v "127.0.0.1" | sed "s^addr:^^g"')
+  echo "${publicHostname} : ${privateIp}" >> /tmp/privateMasterIps
+  if [[ "${privateIp}" = "" ]]; then
+    echo "Could not get a privateIp from one of the master nodes. Waiting and then trying" >> /tmp/bootstrap_log.out
+    sleep 25s
+    privateIp=$(ssh -o "StrictHostKeyChecking=false" systest@${publicHostname} -x 'sudo ifconfig | grep inet | cut -d" " -f 12 | grep "addr:1" | grep -v "127.0.0.1" | sed "s^addr:^^g"')
+    echo "Second attempt at private ip for ${publicHostname} produced: ${privateIp}" >> /tmp/bootstrap_log.out
+  fi
+  echo "Adding to nodes: \"${privateIp}:${NAMEPREFIX}-mn${i}.${CLOUDERA_DOMAIN}:${NAMEPREFIX}-mn${i} \" >> /tmp/bootstrap_log.out"
+  NODES+=("${privateIp}:${NAMEPREFIX}-mn$i.${CLOUDERA_DOMAIN}:${NAMEPREFIX}-mn$i")
+}
 
 #Generate IP Addresses for the cloudera setup
 NODES=()
@@ -54,41 +67,28 @@ let "NAMEEND=MASTERNODES-1" || true
 for i in $(seq 0 $NAMEEND)
 do
   publicHostname=${NAMEPREFIX}-mn$i.${NAMESUFFIX}
-  echo "publicHostname is: ${publicHostname}" >> /tmp/masternodes
-  privateIp=$(ssh -o "StrictHostKeyChecking=false" systest@${publicHostname} -x 'sudo ifconfig | grep inet | cut -d" " -f 12 | grep "addr:1" | grep -v "127.0.0.1" | sed "s^addr:^^g"')
-  echo "${publicHostname} : ${privateIp}" >> /tmp/privateMasterIps
-  if [[ "${privateIp}" = "" ]]; then
-    echo "Could not get a privateIp from one of the master nodes. Waiting and then trying" >> /tmp/initlog.out
-    sleep 25s
-    privateIp=$(ssh -o "StrictHostKeyChecking=false" systest@${publicHostname} -x 'sudo ifconfig | grep inet | cut -d" " -f 12 | grep "addr:1" | grep -v "127.0.0.1" | sed "s^addr:^^g"')
-    echo "Second attempt at private ip for ${publicHostname} produced: ${privateIp}" >> /tmp/initlog.out
-  fi
-  echo "Adding to nodes: \"${privateIp}:${NAMEPREFIX}-mn${i}.${CLOUDERA_DOMAIN}:${NAMEPREFIX}-mn${i} \" >> /tmp/initlog.out"
-  NODES+=("${privateIp}:${NAMEPREFIX}-mn$i.${CLOUDERA_DOMAIN}:${NAMEPREFIX}-mn$i")
+  echo "publicHostname is: ${publicHostname}" >> /tmp/publicHostNames
+  addPrivateIpToNodes ${publicHostname}
 done
 
 let "DATAEND=DATANODES-1" || true
 for i in $(seq 0 $DATAEND)
 do
-  publicHostname=${NAMEPREFIX}-dn$i.${NAMESUFFIX}
-  echo "publicHostname is: ${publicHostname}" >> /tmp/datanodes
-  privateIp=$(ssh -o "StrictHostKeyChecking=false" systest@${publicHostname} -x 'sudo ifconfig | grep inet | cut -d" " -f 12 | grep "addr:1" | grep -v "127.0.0.1" | sed "s^addr:^^g"')
-  echo $privateIp >> /tmp/privateDataIps
-  echo "Adding to nodes: \"${privateIp}:${NAMEPREFIX}-dn$i.${CLOUDERA_DOMAIN}:${NAMEPREFIX}-dn$i \" >> /tmp/initlog.out"
-  NODES+=("${privateIp}:${NAMEPREFIX}-dn$i.${CLOUDERA_DOMAIN}:${NAMEPREFIX}-dn$i")
+  publicHostname=${NAMEPREFIX}-mn$i.${NAMESUFFIX}
+  echo "publicHostname is: ${publicHostname}" >> /tmp/publicHostNames
+  addPrivateIpToNodes ${publicHostname}
 done
 
-echo "finished dn private ip discovery" >> /tmp/bc_initlog.out
-
+# Take the value from NODES and put it into /etc/hosts
 OIFS=$IFS
 IFS=',';NODE_IPS="${NODES[*]}";IFS=$' \t\n'
 
 IFS=','
 for x in $NODE_IPS
 do
-  echo "x as member of NODE_IPS is: $x" >> /tmp/initlog.out
+  echo "x as member of NODE_IPS is: $x" >> /tmp/bootstrap_log.out
   line=$(echo "$x" | sed 's/:/ /' | sed 's/:/ /')
-  echo "$line as member of NODE_IPS is: $x" >> /tmp/initlog.out
+  echo "$line as member of NODE_IPS is: $x" >> /tmp/bootstrap_log.out
   echo "$line" >> /etc/hosts
 done
 IFS=${OIFS}
@@ -100,46 +100,43 @@ mip=$(while read p; do echo $p | grep "azure" | grep -v local | grep "\-mn0" | c
 # Get non-CM nodes
 wip_string=''
 while read p; do
-
   if [[ "$wip_string" != "" ]]; then
-
     wip_string+=','
   fi
   wip_string+=$(echo $p | grep "azure" | grep -v local | grep -v "\-mn0" | cut -d' ' -f 1 );
 done < /etc/hosts
 
-log "mip: $mip"
-log "wip: $wip_string"
-
 # As a final act, we're going to go to each node in /etc/hosts and adjust /etc/hosts and the /etc/resolv.conf
-echo "About to adjust /etc/resolv.conf on all hosts, including this one" >> /tmp/settingResolvConf.out
+echo "About to adjust /etc/resolv.conf on all hosts, including this one" >> /tmp/bootstrap_log.out
 sed -i "s^PEERDNS=yes^PEERDNS=no^g" /etc/sysconfig/network-scripts/ifcfg-eth0
 
+# First do it on the local machine
 sudo echo 'nameserver 172.18.64.15' > /etc/resolv.conf
 sudo service network restart
 sleep 25
 
+# Then set resolv.conf on the others
 while read p; 
 do
   
   host=$(echo $p | grep "azure" | grep -v 'localhost' | grep -v "mn0" | cut -d' ' -f 1)
   
   if [[ "${host}" = "" ]]; then
-    echo "host empty for line $p. continuing" >> /tmp/settingResolvConf.out
+    echo "host empty for line $p. continuing" >> /tmp/bootstrap_log.out
     continue
   fi
 
-  echo "host: ${host}" >> /tmp/settingResolvConf.out
+  echo "host: ${host}" >> /tmp/bootstrap_log.out
   
   scp -o "StrictHostKeyChecking=false" /etc/hosts ${ADMINUSER}@${host}:/home/${ADMINUSER}/hosts
-  echo "done scping to host: ${host}" >> /tmp/settingResolvConf.out
+  echo "done scping to host: ${host}" >> /tmp/bootstrap_log.out
 
   ssh -n -o "StrictHostKeyChecking=false" systest@${host} -x "sudo cp /home/${ADMINUSER}/hosts /etc/hosts; sudo chown root /etc/hosts; sudo chmod 644 /etc/hosts"
-  echo "done setting /etc/hosts on host: ${host}" >> /tmp/settingResolvConf.out
+  echo "done setting /etc/hosts on host: ${host}" >> /tmp/bootstrap_log.out
 
   # set /etc/resolv.conf
   ssh -n -o "StrictHostKeyChecking=false" systest@${host} -x "sudo echo 'nameserver 172.18.64.15' | sudo tee /etc/resolv.conf; sudo sed -i 's^PEERDNS=yes^PEERDNS=no^g' /etc/sysconfig/network-scripts/ifcfg-eth0; sudo service network restart;"
-  echo "done with long command on /etc/hosts on host: ${host}" >> /tmp/settingResolvConf.out
+  echo "done with long command on /etc/hosts on host: ${host}" >> /tmp/bootstrap_log.out
 
 done < /etc/hosts
 sleep 30s
@@ -147,7 +144,7 @@ echo "Done adjusting /etc/resolv.conf on all hosts" >> /tmp/settingResolvConf.ou
 
 while read p; 
 do
-
+  # establish properties necessary to register with DNS
   ip=$(echo $p | grep "azure" | grep -v 'localhost' | cut -d' ' -f 1)
   fqdn=$(echo $p | grep "azure" | grep -v 'localhost' | cut -d' ' -f 2)
   shortname=$(echo $p | grep "azure" | grep -v 'localhost' | cut -d' ' -f 3)
@@ -157,10 +154,8 @@ do
     continue
   fi
 
-  echo "fqdn: ${fqdn}" >> /tmp/settingPrivateHostnames.out
   echo "About to associate ${shortname}.${NAMESUFFIX} to ip ${ip} on domain ${CLOUDERA_DOMAIN}."
   ssh -n -o "StrictHostKeyChecking=no" systest@${CLOUDERA_DNS_IP} -x "./bin/update_dns_multi ${shortname}.${CLOUDERA_DOMAIN} ${ip} ${CLOUDERA_DOMAIN}"
- 
 done < /etc/hosts
 
 # This key should have been set by initialize-node.sh
