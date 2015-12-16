9d859210c210ac167f3e60ac70d6cb3d6263bf51
Aleksandr Shulman <aleks@cloudera.com>
2015-12-16 12:11:19 -0800

    Update initialize-node.sh

--- cloudera-on-centos/scripts/initialize-node.sh
+++ cloudera-on-centos/scripts/initialize-node.sh
@@ -61,13 +61,13 @@ sudo chmod 700 ~/.ssh
 
 cp /etc/resolv.conf /tmp/old_resolv.conf
 sudo echo "nameserver 172.18.64.15" > /etc/resolv.conf
-sleep 10s
+sleep 30s
 cat /etc/resolv.conf >> /tmp/diagnostics.out
 
 # set the configuration to not reset /etc/resolv.conf when we restart networking
 sed -i "s^PEERDNS=yes^PEERDNS=no^g" /etc/sysconfig/network-scripts/ifcfg-eth0
 service network restart
-sleep 50s
+sleep 100s
 wget --no-dns-cache http://github.mtv.cloudera.com/raw/QE/smokes/cdh5/common/src/main/resources/systest/id_rsa
 statusCode=$?
 if [[ "$statusCode" != "0" ]]; then
